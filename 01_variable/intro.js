bb68a0dad356bd6d49b00f3e23f3ea69b2c1b2d3
mdabdulhalim-bd <halim.zoomit@gmail.com>
2025-03-22 11:20:10 +0600

    JavaScript MaroThaba

---
+++ 01_variable/intro.js
@@ -0,0 +1,24 @@
+const weight = 38
+const price = 760
+const year = 2025
+const balance = 2000
+const age = 29
+
+
+// Practice Problem
+// 1
+const DOB = "01-01-2025"
+// 2
+const siblings = 5; 
+//3
+const students = 200;
+//4
+const marks = 70;
+//5
+const rooms = 3;
+//6
+const dailyReadingTime = 6;
+//7
+const total = 0 + 1 + 7 + 7 + 0 + 7 + 2 + 7+ 9 + 2 + 5;
+console.log(total)
+
