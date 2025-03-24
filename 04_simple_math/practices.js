60bbc8903036525c57e98550e90642106fc16519
mdabdulhalim-bd <halim.zoomit@gmail.com>
2025-03-24 21:58:04 +0600

    updated course

---
+++ 04_simple_math/practices.js
@@ -0,0 +1,29 @@
+
+// Find the total amount of eggPrice and Onion price and print
+
+const eggPrice = 10;
+const onionPrice = 20;
+const total = eggPrice + onionPrice;
+console.log("Total Amount of Egg and Onion  = " + total);
+
+// Find the difference between Egg price and Onion price and showing it in clg
+
+const result = eggPrice - onionPrice;
+console.log(result);
+
+// Add two string
+const first = "Abdul"
+const last = " Halim"
+console.log(first + last);
+
+const daddy = 20;
+const sugar = '20'
+console.log(daddy + sugar);
+
+
+// Find the discount
+const price = 35;
+const inboxEAsenPrice = price + 10;
+const newPrice = price + 50;
+const discountedPrice = newPrice - 10;
+console.log(discountedPrice);
---
+++ 04_simple_math/problem_set_01.js
@@ -0,0 +1,6 @@
+
+//01
+const income = 100;
+const expense = 40;
+const remainingBalance = income - expense;
+console.log("Remaining Taka : " + remainingBalance);
